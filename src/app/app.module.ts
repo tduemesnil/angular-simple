48a0ebed4c27be27b1a73231b6ac383de4fdd6a2
Thomas Duemesnil <tduemesnil@barnet-europe.com>
2018-04-10 12:46:15 +0200

    Erste Schritt läuft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { JokeComponent } from './joke/joke.component';
+import { JokeListComponent } from './joke-list/joke-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent,
+    JokeListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/joke/joke.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Joke } from '../joke';
+
+@Component({
+  selector: 'app-joke',
+  templateUrl: './joke.component.html',
+  styleUrls: ['./joke.component.css']
+})
+export class JokeComponent implements OnInit {
+  @Input() joke: Joke;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
